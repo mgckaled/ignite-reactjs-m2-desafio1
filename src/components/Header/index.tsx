42253cb0b1279dfd1c5689b3f3ad1e9011c171ce
mgckaled <marcelgckaled@gmail.com>
2022-07-15 19:42:21 -0300

    update challenge

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { MdShoppingBasket } from 'react-icons/md';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { MdShoppingBasket } from 'react-icons/md'
 
-import logo from '../../assets/images/logo.svg';
-import { Container, Cart } from './styles';
-import { useCart } from '../../hooks/useCart';
+import logo from '../../assets/images/logo.svg'
+import { Container, Cart } from './styles'
+import { useCart } from '../../hooks/useCart'
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+	const { cart } = useCart()
+	// o tamanho do carrinho é a quantidade de produtos
+	const cartSize = cart.length
 
-  return (
-    <Container>
-      <Link to="/">
-        <img src={logo} alt="Rocketshoes" />
-      </Link>
+	return (
+		<Container>
+			<Link to="/">
+				<img src={logo} alt="Rocketshoes" />
+			</Link>
 
-      <Cart to="/cart">
-        <div>
-          <strong>Meu carrinho</strong>
-          <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
-          </span>
-        </div>
-        <MdShoppingBasket size={36} color="#FFF" />
-      </Cart>
-    </Container>
-  );
-};
+			<Cart to="/cart">
+				<div>
+					<strong>Meu carrinho</strong>
+					<span data-testid="cart-size">
+						{/* algorítimo ternário para incluir total de itens no plural */}
+						{cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
+					</span>
+				</div>
+				<MdShoppingBasket size={36} color="#FFF" />
+			</Cart>
+		</Container>
+	)
+}
 
-export default Header;
+export default Header
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,75 +1,123 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from 'react'
+import { toast } from 'react-toastify'
+import { api } from '../services/api'
+import { Product, Stock } from '../types'
 
+/**
+ * A React Node is one of the following types:
+ * Boolean (which is ignored)
+ * null or undefined (which is ignored)
+ * Number
+ * String
+ * A React element (result of JSX)
+ * An array of any of the above, possibly a nested one
+ **/
 interface CartProviderProps {
-  children: ReactNode;
+	children: ReactNode
 }
 
 interface UpdateProductAmount {
-  productId: number;
-  amount: number;
+	productId: number
+	amount: number
 }
 
 interface CartContextData {
-  cart: Product[];
-  addProduct: (productId: number) => Promise<void>;
-  removeProduct: (productId: number) => void;
-  updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
+	cart: Product[]
+	addProduct: (productId: number) => Promise<void>
+	removeProduct: (productId: number) => void
+	updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void
 }
 
-const CartContext = createContext<CartContextData>({} as CartContextData);
+const CartContext = createContext<CartContextData>({} as CartContextData)
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
-  const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+	const [cart, setCart] = useState<Product[]>(() => {
+		const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+		// conversão do valor registrado em 'string'
+		// O array de produtos será transformado em 'string'
+		if (storagedCart) {
+			return JSON.parse(storagedCart)
+		}
 
-    return [];
-  });
+		// se o valor for 'null' retorna array vazio
+		return []
+	})
 
-  const addProduct = async (productId: number) => {
-    try {
-      // TODO
-    } catch {
-      // TODO
-    }
-  };
+	const addProduct = async (productId: number) => {
+		try {
+			// criar um novo array com os valores de 'Cart' para garantir imutabilidade
+			const updatedCart = [...cart]
 
-  const removeProduct = (productId: number) => {
-    try {
-      // TODO
-    } catch {
-      // TODO
-    }
-  };
+			// Verificação se o produt.id iterado = ao produto.id da função
+			const productExists = updatedCart.find(
+				product => product.id === productId
+			)
 
-  const updateProductAmount = async ({
-    productId,
-    amount,
-  }: UpdateProductAmount) => {
-    try {
-      // TODO
-    } catch {
-      // TODO
-    }
-  };
+			//
+			const stock = await api.get(`/stock/${productId}`)
 
-  return (
-    <CartContext.Provider
-      value={{ cart, addProduct, removeProduct, updateProductAmount }}
-    >
-      {children}
-    </CartContext.Provider>
-  );
+			const stockAmount = stock.data.amount
+
+			const currentAmount = productExists ? productExists.amount : 0
+
+			// quantidade desejada
+			const amount = currentAmount + 1
+
+			if (amount > stockAmount) {
+				toast.error('Quantidade solicitada fora de estoque')
+				return
+			}
+
+			if (productExists) {
+				productExists.amount = amount
+			} else {
+				const product = await api.get(`/products/${productId}`)
+
+				const newProduct = {
+					...product.data,
+					amount: 1
+				}
+				updatedCart.push(newProduct)
+			}
+
+			setCart(updatedCart)
+			localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+		} catch {
+			toast.error('Erro na adição do produto')
+		}
+	}
+
+	const removeProduct = (productId: number) => {
+		try {
+			// TODO
+		} catch {
+			// TODO
+		}
+	}
+
+	const updateProductAmount = async ({
+		productId,
+		amount
+	}: UpdateProductAmount) => {
+		try {
+			// TODO
+		} catch {
+			// TODO
+		}
+	}
+
+	return (
+		<CartContext.Provider
+			value={{ cart, addProduct, removeProduct, updateProductAmount }}
+		>
+			{children}
+		</CartContext.Provider>
+	)
 }
 
 export function useCart(): CartContextData {
-  const context = useContext(CartContext);
+	const context = useContext(CartContext)
 
-  return context;
+	return context
 }
